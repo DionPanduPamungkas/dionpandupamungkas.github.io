31d5c83749321f4a0c4093efb9249d01caeb7225
Yonn-2k03 <dionpandu9613@gmail.com>
2022-11-16 18:56:38 +0700

    Tambah File

---
+++ index.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.number1', { delay: 600 })
+ScrollReveal().reveal('.number2', { delay: 900 })
+ScrollReveal().reveal('.number3', { delay: 1200 })
+ScrollReveal().reveal('.number4', { delay: 1500 })
+ScrollReveal().reveal('.number5', { delay: 1800 })
+ScrollReveal().reveal('.number6', { delay: 2100 })
+ScrollReveal().reveal('.number7', { delay: 2400 })
+
